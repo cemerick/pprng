9f94f45398d944c63a6a61af9caa9108c60b5443
Chas Emerick <chas@cemerick.com>
2013-10-12 13:38:38 -0400

    dep bumps, no longer need a copy of the clojurescript.test runner here

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                    :output-path "target/test-classes"
                    :rules :cljs}]}
 
-  :cljsbuild {:test-commands {"phantom" ["runners/phantomjs.js" "target/testable.js"]}
+  :cljsbuild {:test-commands {"phantom" ["phantomjs" :runner "target/testable.js"]}
               :builds [{:source-paths ["target/classes" "target/test-classes"
                                        "src/resources"]
                         :compiler {:output-to "target/testable.js"
@@ -30,23 +30,13 @@
                                    :optimizations :advanced
                                    :pretty-print true}}]}
 
-  :profiles {:dev {:dependencies [[com.cemerick/clojurescript.test "0.0.4"]
-                                  [com.cemerick/piggieback "0.0.5-SNAPSHOT"]
-                                  [org.clojars.cemerick/cljx "0.3.0-SNAPSHOT"]]
-                   :plugins [[org.clojars.cemerick/cljx "0.3.0-SNAPSHOT"]
-                             ; temp fix https://github.com/emezeske/lein-cljsbuild/issues/210
-                             [org.clojars.cemerick/lein-cljsbuild "0.3.2.1"]]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.0.5-SNAPSHOT"]
+                                  [com.keminglabs/cljx "0.3.0"]]
+                   :plugins [[com.cemerick/clojurescript.test "0.1.0"]
+                             [com.keminglabs/cljx "0.3.0"]
+                             [lein-cljsbuild "0.3.3"]]
                    :aliases {"cleantest" ["do" "clean," "cljx" "once," "test,"
-                                          "cljsbuild" "once," "cljsbuild" "test"]}
+                                          "cljsbuild" "test"]}
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl
-                                                     cljx.repl-middleware/wrap-cljx]}
-                   #_#_
-                   :injections [(require '[cljs.repl.browser :refer (exec-env)]
-                                         '[cemerick.piggieback :refer (cljs-repl)])
-                                (defn browser-repl []
-                                  (cljs-repl :repl-env
-                                             ; requires fix for http://dev.clojure.org/jira/browse/CLJS-521
-                                             (doto (exec-env :libs [""])
-                                               ; TODO https://github.com/cemerick/piggieback/issues/10
-                                               cljs.repl/-setup)))]}})
+                                                     cljx.repl-middleware/wrap-cljx]}}})
 
